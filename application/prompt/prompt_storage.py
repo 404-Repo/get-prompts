1ce915ef4fcf74ea9b724b4183b7846a195f1ef8
Raman Kudaktsin <raman@404.xyz>
2025-04-18 11:39:03 +0300

    fix: fix image prompt storage

--- application/prompt/prompt_storage.py
+++ application/prompt/prompt_storage.py
@@ -91,7 +91,7 @@ class InMemoryImagePromptStorage(BasePromptStorage):
 
     def __init__(self, *, max_url_cnt: int, default_image_url_file_path: Path) -> None:
         self._urls: deque[str] = deque(maxlen=max_url_cnt)
-        self._url_to_prompt: dict[str, str | None] = dict()
+        self._url_to_normalized_prompt: dict[str, str] = dict()
         if default_image_url_file_path.exists():
             df = pd.read_csv(default_image_url_file_path)
             self._url_to_normalized_prompt = df.set_index(df.columns[0])[df.columns[1]].to_dict()
