272b7404426273a561618bc9f4b68cc1a0b8c74d
Raman Kudaktsin <raman@404.xyz>
2025-09-26 16:57:29 +0300

    fix: remove debug code

--- metagraph/metagraph.py
+++ metagraph/metagraph.py
@@ -45,10 +45,6 @@ class Metagraph:
 
         keypair = Keypair(ss58_address=hotkey)
         message = f"{nonce}{hotkey}"
-        config = self.config
-        wallet = bt.wallet(config=config)
-        logger.info(f"message: {base64.b64encode(wallet.hotkey.sign(message)).decode(encoding="utf-8")}")
-        logger.info(f"signature: {wallet.hotkey.ss58_address}")
         try:
             result = bool(keypair.verify(message, base64.b64decode(signature.encode(encoding="utf-8"))))
         except Exception as e:
